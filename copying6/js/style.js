12464d7e53251777dd2e3ffba785fc73594461dd
Tomoryo211 <ryo.tomo.211@gmail.com>
2025-07-15 13:45:53 +0900

    todoリスト練習

---
+++ copying6/js/style.js
@@ -0,0 +1,32 @@
+
+
+const textInput = document.getElementById("text-input");
+const todoList = document.getElementById("TodoList")
+
+textInput.addEventListener("keydown",e => {
+    const text = textInput.value.trim();
+    if(text === " " || e.key !== "Enter"){
+        return;
+    }
+
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    const button = document.createElement("button");
+
+    li.classList.add("Listitem");
+    span.textContent = text;
+    span.classList.add("Todotext");
+
+    button.textContent = "削除";
+    button.type = "button";
+    button.classList.add("delete_button");
+    button.addEventListener("click",e => {
+        TodoList.removeChild(e.target.closest("li"));
+    })
+
+    li.appendChild(span);
+    li.appendChild(button);
+    TodoList.appendChild(li);
+
+    textInput.value = "";
+});
